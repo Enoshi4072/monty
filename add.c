4299193470cd93d53b5176a429692c0c3b6eca28
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-21 19:22:41 +0000

    stacks

--- add.c
+++ add.c
@@ -10,14 +10,13 @@
 
 void add(stack_t **stack)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "Error: can't add, stack too short\n");
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->n += (*stack)->n;
-    pop(stack);
+if (*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "Error: can't add, stack too short\n");
+exit(EXIT_FAILURE);
+}
+(*stack)->next->n += (*stack)->n;
+pop(stack);
 }
 
 
