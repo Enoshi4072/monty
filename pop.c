b2d249fdb42e9163a5bf3d9b9d8ff7cf83f7fa53
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-22 22:45:02 +0000

    2.pop

--- pop.c
+++ pop.c
@@ -6,7 +6,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	stack_t *top;
     if (*stack == NULL)
     {
-        printf("L%u: can't pop an empty stack\n", line_number);
+        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
         exit(EXIT_FAILURE);
     }
 
