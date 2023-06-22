6f63580c7820a6520de8311c74ad5c99cd8f06b5
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-22 21:59:42 +0000

    pop

--- monty.h
+++ monty.h
@@ -23,6 +23,5 @@ void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
 void execute_instruction(stack_t **stack, char *opcode, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
-
-
+void pop(stack_t **stack, unsigned int line_number);
 #endif
