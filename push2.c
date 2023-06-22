b9753dd88af29ba7765e1ea7f6cfdbae4df8ce2a
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-22 07:48:50 +0000

    str

--- push2.c
+++ push2.c
@@ -1,31 +1,60 @@
 #include "monty.h"
-void push(stack_t **stack, int value)
+
+/**
+ * push - Pushes an element to the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number being executed.
+ * @n: Value to push.
+ */
+void push(stack_t **stack, unsigned int line_number, int n)
 {
     stack_t *new_node = malloc(sizeof(stack_t));
     if (new_node == NULL)
     {
         fprintf(stderr, "Error: malloc failed\n");
+        free_stack(*stack);
         exit(EXIT_FAILURE);
     }
 
-    new_node->n = value;
+    new_node->n = n;
     new_node->prev = NULL;
-    new_node->next = *stack;
 
     if (*stack != NULL)
         (*stack)->prev = new_node;
 
+    new_node->next = *stack;
     *stack = new_node;
 }
 
-void pall(stack_t **stack)
+/**
+ * pall - Prints all the values on the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number being executed.
+ */
+void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+    stack_t *temp = *stack;
 
-    while (current != NULL)
+    while (temp != NULL)
     {
-        printf("%d\n", current->n);
-        current = current->next;
+        printf("%d\n", temp->n);
+        temp = temp->next;
+    }
+}
+
+/**
+ * free_stack - Frees a stack.
+ * @stack: Pointer to the stack.
+ */
+void free_stack(stack_t *stack)
+{
+    stack_t *temp;
+
+    while (stack != NULL)
+    {
+        temp = stack;
+        stack = stack->next;
+        free(temp);
     }
 }
 
